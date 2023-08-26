fce05451ab68e18e24a6ff9a9f4e9d3f41e424cb
LakshyaSvt <142570590+LakshyaSvt@users.noreply.github.com>
2023-08-26 14:50:19 +0530

    Update ServiceLead.php

--- app/Models/ServiceLead.php
+++ app/Models/ServiceLead.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class ServiceLead extends Model
 {
     use HasFactory;
+
+    public function scopeCityId($query)
+    {
+        if (Auth::check()) {
+            $user = Auth::user();
+            //check if user has employee role
+            if ($user->hasRole('employee')) {
+                return $query->where('city_id', $user->city_id);
+            }
+        }
+    }
 }
